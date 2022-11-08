84ec35b9862cdcdc32eaae3ddc6fc5ad7cdbe9f8
Junseo Oh <insung9546@gmail.com>
2022-11-08 17:44:29 +0900

    Fix: add error

--- api/image/index.js
+++ api/image/index.js
@@ -1,15 +1,23 @@
 // @ts-check
+const renderError = require("../../render/error");
+
 module.exports = async (req, res) => {
   const b64String = req.query.base64 || "";
-  var byteString = atob(b64String);
-  var arrayBuffer = new ArrayBuffer(byteString.length);
-  var intArray = new Uint8Array(arrayBuffer);
-  for (var i = 0; i < byteString.length; i++) {
-    intArray[i] = byteString.charCodeAt(i);
-  }
-  var imageBlob = new Blob([intArray], { type: "image/png" });
+  console.log("b64String: ", b64String);
+  try {
+    var byteString = atob(b64String);
+    var arrayBuffer = new ArrayBuffer(byteString.length);
+    var intArray = new Uint8Array(arrayBuffer);
+    for (var i = 0; i < byteString.length; i++) {
+      intArray[i] = byteString.charCodeAt(i);
+    }
+    var imageBlob = new Blob([intArray], { type: "image/png" });
 
-  res.setHeader("Content-Type", "image/png");
-  res.setHeader("Content-Length", imageBlob.size);
-  res.send(imageBlob);
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Content-Length", imageBlob.size);
+    res.send(imageBlob);
+  } catch (e) {
+    console.log("e : ", e);
+    return res.send(renderError());
+  }
 };
