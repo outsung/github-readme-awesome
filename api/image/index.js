a6a95ce6d6d55f0aa5071e84ee12453e18346beb
Junseo Oh <insung9546@gmail.com>
2022-11-16 13:07:15 +0900

    Fix: remove `data:image/png;base64,`

--- api/image/index.js
+++ api/image/index.js
@@ -4,8 +4,10 @@ const renderError = require("../../render/error");
 
 module.exports = async (req, res) => {
   const b64String = req.query.base64 || "";
-  let decode = Buffer.from(decodeURIComponent(b64String), "base64");
-
+  let decode = Buffer.from(
+    decodeURIComponent(b64String).split(",")[1],
+    "base64"
+  );
   res.setHeader("Content-Type", "image/png");
   res.setHeader("Content-Length", decode.length);
   res.send(decode);
