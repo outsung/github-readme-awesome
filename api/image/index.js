652c18a334ea9270ef77c2a64898f6a5cf2ca82c
Junseo Oh <insung9546@gmail.com>
2022-11-16 12:59:21 +0900

    Feat: add decodeURIComponent

--- api/image/index.js
+++ api/image/index.js
@@ -4,7 +4,7 @@ const renderError = require("../../render/error");
 
 module.exports = async (req, res) => {
   const b64String = req.query.base64 || "";
-  let decode = Buffer.from(b64String, "base64");
+  let decode = Buffer.from(decodeURIComponent(b64String), "base64");
 
   res.setHeader("Content-Type", "image/png");
   res.setHeader("Content-Length", decode.length);
