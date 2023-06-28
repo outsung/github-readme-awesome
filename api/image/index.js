f5f363a1e60d0237be60326c639e312bff5a2830
Junseo Oh <40460655+outsung@users.noreply.github.com>
2023-06-28 16:01:47 +0900

    Feat: add isDownload

--- api/image/index.js
+++ api/image/index.js
@@ -3,28 +3,9 @@ const buffer = require("buffer");
 const renderError = require("../../render/error");
 const QRCode = require("qrcode");
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
 const handler = async (req, res) => {
   const url = req.query.url || "";
+  const isDownload = req.query.isDownload || false;
 
   let decode = Buffer.from(
     (await QRCode.toDataURL(decodeURIComponent(url))).split(",")[1],
@@ -32,7 +13,8 @@ const handler = async (req, res) => {
   );
   res.setHeader("Content-Type", "image/png");
   res.setHeader("Content-Length", decode.length);
-  res.send(decode);
+  isDownload && res.setHeader("Content-Disposition", "attachment");
+  res.res.send(decode);
 };
 
-module.exports = allowCors(handler);
+module.exports = handler;
